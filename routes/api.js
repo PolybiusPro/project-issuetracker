641b89d3b698a9fbc935e0c4c7c90cb2689c0230
Wes Sitzes <polybiuspro@protonmail.com>
2025-02-19 15:48:25 -0500

    feat: add routes

--- routes/api.js
+++ routes/api.js
@@ -1,27 +1,28 @@
-'use strict';
+"use strict";
+
+const {
+    getIssues,
+    postIssue,
+    putIssue,
+    deleteIssue,
+} = require("../controllers/issueController.js");
 
 module.exports = function (app) {
+    app.route("/api/issues/:project")
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .post(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .put(function (req, res){
-      let project = req.params.project;
-      
-    })
-    
-    .delete(function (req, res){
-      let project = req.params.project;
-      
-    });
-    
+        .get(function (req, res) {
+            getIssues(req, res);
+        })
+
+        .post(function (req, res) {
+            postIssue(req, res);
+        })
+
+        .put(function (req, res) {
+            putIssue(req, res);
+        })
+
+        .delete(function (req, res) {
+            deleteIssue(req, res);
+        });
 };
