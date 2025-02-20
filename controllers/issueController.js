2b15b6f247768edae7c1d2ade29c93d2d989ba1b
Wes Sitzes <polybiuspro@protonmail.com>
2025-02-20 15:50:24 -0500

    feat: create PUT and DELETE controllers

--- controllers/issueController.js
+++ controllers/issueController.js
@@ -29,7 +29,7 @@ const postIssue = async (req, res) => {
         );
         res.json(issue);
     } catch (err) {
-        res.status(400).json({ error: "Missing required field(s)" });
+        res.status(200).json({ error: "Missing required field(s)" });
     }
 };
 
@@ -56,4 +56,74 @@ const getIssues = async (req, res) => {
     }
 };
 
-module.exports = { postIssue, getIssues };
+const putIssue = async (req, res) => {
+    if (!req.body._id) {
+        return res.json({ error: "missing _id" });
+    }
+
+    try {
+        const projectQuery = await Project.findOne({
+            name: req.params.project,
+        });
+
+        const issueToEdit = projectQuery.issues.id(req.body._id);
+
+        if (!issueToEdit) {
+            return res.json({
+                _id: req.body._id,
+                error: "could not update",
+            });
+        }
+
+        const fieldsToEdit = req.body;
+
+        Object.keys(fieldsToEdit).forEach((field) => {
+            if (field !== "_id") {
+                issueToEdit[field] = fieldsToEdit[field];
+            }
+        });
+
+        await projectQuery.save();
+        res.json({
+            _id: req.body._id,
+            result: "successfully updated",
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Server error" });
+    }
+};
+
+const deleteIssue = async (req, res) => {
+    if (!req.body._id) {
+        return res.json({ error: "missing _id" });
+    }
+
+    try {
+        const projectQuery = await Project.findOne({
+            name: req.params.project,
+        });
+
+        const issueToDelete = projectQuery.issues.id(req.body._id);
+
+        if (!issueToDelete) {
+            return res.json({
+                _id: req.body._id,
+                error: "could not delete",
+            });
+        }
+
+        issueToDelete.deleteOne();
+
+        await projectQuery.save();
+        res.json({
+            _id: req.body._id,
+            result: "successfully deleted",
+        });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Server error" });
+    }
+};
+
+module.exports = { postIssue, getIssues, putIssue, deleteIssue };
